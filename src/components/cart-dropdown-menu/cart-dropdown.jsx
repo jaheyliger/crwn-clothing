17840a5ab152645f5f2b788203f39ead464c86c1
jayvon <jaheyliger@yahoo.com>
2021-01-27 16:16:59 -0500

    added the cart dropdown component to update with cart items and quantities

--- src/components/cart-dropdown-menu/cart-dropdown.jsx
+++ src/components/cart-dropdown-menu/cart-dropdown.jsx
@@ -1,15 +1,26 @@
 import React from 'react'
 import CustomButton from '../custom-button/custom-button.component'
 import './cart-dropdown.styles.scss'
+import CartItem from '../cart-item/cart-item'
+import {connect} from 'react-redux'
 
-const CartDropdown = () => {
+const CartDropdown = ({ cartItems }) => {
     return (
         <div className='cart-dropdown'>
             <div className="cart-items">
-                <CustomButton>Go To Checkout</CustomButton>
+                {
+                    cartItems.map(cartItem => (
+                         <CartItem key={cartItem.id} item={cartItem} />
+                    ))
+                }
             </div>
+            <CustomButton>Go To Checkout</CustomButton>
         </div>
     )
 }
 
-export default CartDropdown
+const mapStateToProps = ({cart: {cartItems}}) => ({
+    cartItems
+})
+
+export default connect(mapStateToProps)(CartDropdown)
